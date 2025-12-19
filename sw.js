e66a7ddd2ee3d8f03d2725711f5b6944791fcdcb
organixer0 <organixer0@gmail.com>
2025-12-19 18:15:58 +0000

    FC-arabian

---
+++ sw.js
@@ -0,0 +1,7 @@
+﻿self.addEventListener("install",e=>{
+  e.waitUntil(
+    caches.open("arebian").then(c=>c.addAll([
+      "./","./index.html","./style.css","./script.js","./logo.png"
+    ]))
+  );
+});
